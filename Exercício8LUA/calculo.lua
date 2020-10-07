561b5b3e3f4f9ab88f7b4d6b451ffd98491fc4b5
Giuseppe Ugo Zapparoli <zapparoli.giuseppe@gmail.com>
2020-10-07 17:44:27 -0300

    Teste

---
+++ Exercício8LUA/calculo.lua
@@ -0,0 +1,4 @@
+function calculo(r)
+    local ve = (4*3.14*r^3)/3
+    return calculo
+end
---
+++ Exercício8LUA/index.lua
@@ -0,0 +1,4 @@
+print("Esse programa calcula o volume de uma esfera.")
+
+function calculo(5)
+print("O valor do volume da esfera é: " .. calculo())
